e2d927edef27cac3251673257e56f6eef7f91599
Andrew Tran <andrewtttran@gmail.com>
2020-06-04 21:15:53 +1000

    added image slider, bit janky though

--- kanye/kanye.js
+++ kanye/kanye.js
@@ -1,9 +1,26 @@
-// fetch().then((response) => {
-//   return response;
-// });
+console.log("Hello");
 
-// console.log(axios);
+function kanyeQuote() {
+  axios.get("https://api.kanye.rest").then(function (response) {
+    console.log(response.data);
+    alert(response.data.quote);
+    // window.alert(response.data.quote);
+    // document.write(response.data.quote); // not useful, deletes button
+  });
+}
 
-// axios.get("https://api.kanye.rest").then(function (response) {
-//   console.log(response.data);
-// });
+// config to send headers
+const config = {
+  headers: {
+    Accept: "application/json",
+  },
+};
+
+function dadJoke() {
+  axios
+    .get("https://icanhazdadjoke.com/", config)
+    .then((res) => {
+      console.log(res.data), alert(res.data.joke);
+    })
+    .catch((err) => console.error(err));
+}

b815cb5c41bd0e13cc0a38e31eed4951c265f4de
Andrew Tran <andrewtttran@gmail.com>
2020-06-08 21:54:13 +1000

    pokedex app following James Q Quick yt vid

---
+++ pokedex/app.js
@@ -0,0 +1,37 @@
+const pokedex = document.getElementById("pokedex");
+console.log(pokedex);
+
+const fetchPokemon = () => {
+  const promises = [];
+  for (let i = 1; i <= 5; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+  }
+  Promise.all(promises).then((results) => {
+    const pokemon = results.map((data) => ({
+      //the map function will create a new array of our data and convert it to how we specified, in this instance it should create a pokemon object with the following properties
+      name: data.name,
+      id: data.id,
+      image: data.sprites.front_default,
+      type: data.types.map((type) => type.type.name).join(", "),
+    }));
+    displayPokemon(pokemon);
+  });
+};
+
+const displayPokemon = (pokemon) => {
+  console.log(pokemon);
+  const pokemonHTMLString = pokemon
+    .map(
+      (pokeman) => `
+    <li class="card">
+        <img class="card-image" src="${pokeman.image}"/>
+        <h2 class="card-title"> ${pokeman.id}. ${pokeman.name}</h2>
+        <p class= "card-subtitle">Type: ${pokeman.type}</p>
+    </li>`
+    )
+    .join("");
+  pokedex.innerHTML = pokemonHTMLString; //setting the innerHTML property, pokedex is our order list
+};
+
+fetchPokemon();
